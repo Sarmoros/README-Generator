e5fc245c35d631486a76c3b55ad2ef069839493b
Sarahy M <sarahym0703@gmail.com>
2023-11-30 15:50:25 -0500

    added questions for user input

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,53 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "Provide a short description explaining the what, why, and how of your project."
+},
+{
+    type: "input",
+    name: "installation instructions",
+    message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+},
+{
+    type: "input",
+    name: "Usage information",
+    message: "Provide instructions and examples for use."
+},
+{
+    type: "input",
+    name: "contribution guidelines",
+    message: "Enter collaborators, if any, and any other sources used."
+},
+{
+    type: "input",
+    name: "test instructions",
+    message: "Write tests for your application."
+},
+{
+    type: "list",
+    name: "license",
+    message: "Choose your license:"
+},
+{
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username."
+},
+{
+    type: "input",
+    name: "email",
+    message: "Enter your email address."
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
